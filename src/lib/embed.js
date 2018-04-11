0e8c20f53c249cce2978c3e2924fc15b17f12b07
Sumanth Srinivasan <sumanth@vimeo.com>
2018-04-11 09:58:57 -0400

    Remove vimeo-space conditional

--- src/lib/embed.js
+++ src/lib/embed.js
@@ -181,11 +181,10 @@ export function resizeEmbeds(parent = document) {
                 continue;
             }
 
+            // Change padding-bottom of the enclosing div to accommodate
+            // card carousel without distorting aspect ratio
             const space = iframes[i].parentElement;
-
-            if (space && space.className.indexOf('vimeo-space') !== -1) {
-                space.style.paddingBottom = `${event.data.data[0].bottom}px`;
-            }
+            space.style.paddingBottom = `${event.data.data[0].bottom}px`;
 
             break;
         }
